ec4ca5b05945c79129b202f7bf100402d7b07e8c
yaoruo <125192289+yaoruo@users.noreply.github.com>
2024-11-07 17:36:10 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // 在生产构建时忽略 ESLint 错误
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // 在生产构建时忽略 TypeScript 错误
-    ignoreBuildErrors: true,
-  },
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+export default nextConfig;
