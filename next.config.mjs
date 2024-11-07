908321e90e769346ceb13c9ac8d08719971bfc82
yaoruo <125192289+yaoruo@users.noreply.github.com>
2024-11-07 17:34:03 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
     // 在生产构建时忽略 ESLint 错误
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // 在生产构建时忽略 TypeScript 错误
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
