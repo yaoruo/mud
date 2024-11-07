89cbb141ce89090a55de7213fb1f6044516d8530
yaoruo <125192289+yaoruo@users.noreply.github.com>
2024-11-07 17:36:38 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // 在生产构建时忽略 ESLint 错误
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // 在生产构建时忽略 TypeScript 错误
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
